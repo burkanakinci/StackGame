88d80e05ebb2f37512d70dfbee6486d22f4f43dd
DESKTOP-3625S7R\brkna <brkn.aknc4@gmail.com>
2022-03-14 08:26:09 +0300

    no message

--- Assets/ProjeAciklama.cs
+++ Assets/ProjeAciklama.cs
@@ -1,3 +1,4 @@
 //PolygonArsenal ve ToonWater Paketleri görsel zenginlik kazandırmak amacıyla kullanılmıştır.
 //Projede Singleton, Observer, Flyweight tasarım desenleri kullanılmıştır.
 //LevelData Creator sahnesi level oluşturmak için hazırlanmıştır.
+//APK Build eklenmemiştir.
